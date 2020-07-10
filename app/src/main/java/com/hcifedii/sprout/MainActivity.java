543d6ff1b79c061be0907d4c4b1c89e7776cabbd
Juuzen <mr.developer@outlook.it>
2020-07-10 19:36:53 +0200

    Trying to create the BAB

--- app/src/main/java/com/hcifedii/sprout/MainActivity.java
+++ app/src/main/java/com/hcifedii/sprout/MainActivity.java
@@ -3,6 +3,10 @@ package com.hcifedii.sprout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.firstMenuItem:
+                Toast.makeText(this, "Sono il primo", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.secondMenuItem:
+                Toast.makeText(this, "Sono il secondo", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
