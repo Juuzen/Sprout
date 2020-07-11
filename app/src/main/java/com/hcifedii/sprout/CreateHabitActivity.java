94b0ceab4aa4ae0f18fc0fd66ad728ba6a7dc099
HttpSal <salvatore.cuomo4@gmail.com>
2020-07-11 17:24:46 +0200

    Split activity_create_habit into multiple smaller layout

--- app/src/main/java/com/hcifedii/sprout/CreateHabitActivity.java
+++ app/src/main/java/com/hcifedii/sprout/CreateHabitActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CreateHabitActivity extends AppCompatActivity {
 
@@ -16,15 +22,40 @@ public class CreateHabitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_habit);
 
         setTopBarBackButton(true);
+
+        ExtendedFloatingActionButton saveFab = findViewById(R.id.fabSaveButton);
+        saveFab.setOnClickListener(view -> {
+
+            TextInputLayout titleLayout = findViewById(R.id.titleLayout);
+            EditText titleEditText = findViewById(R.id.titleField);
+
+            String title = titleEditText.getText().toString();
+
+            if (title.length() > 0) {
+
+                // Clear error message
+                titleLayout.setError(null);
+
+                // Save habit
+
+            } else {
+                titleLayout.setError(getString(R.string.titleIsEmptyError));
+            }
+        });
+
+
+
+
+
     }
 
 
-    private void setTopBarBackButton(boolean flag){
+    private void setTopBarBackButton(boolean flag) {
 
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
+        if (actionBar != null) {
             actionBar.setHomeButtonEnabled(flag);
-        }else{
+        } else {
             Log.e(logcatTag, "getSupportActionBar() returned null");
         }
     }
