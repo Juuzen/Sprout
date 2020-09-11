fb7f5a8b8d2e11bc9f764fe37384d80fec453641
HttpSal <salvatore.cuomo4@gmail.com>
2020-09-11 13:16:51 +0200

    Added streak chart

--- app/src/main/java/com/hcifedii/sprout/HabitStatsActivity.java
+++ app/src/main/java/com/hcifedii/sprout/HabitStatsActivity.java
@@ -13,12 +13,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.BarLineChartBase;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.textview.MaterialTextView;
 import com.hcifedii.sprout.enumerations.GoalType;
 import com.hcifedii.sprout.enumerations.HabitType;
 
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Comparator;
@@ -31,7 +47,7 @@ public class HabitStatsActivity extends AppCompatActivity {
 
     public static final String EXTRA_HABIT_ID = "habitID";
 
-    private final String secondaryColorHex = "#FDC210";
+    private static final DecimalFormat format = new DecimalFormat("###");
 
     private Habit habit;
 
@@ -51,7 +67,7 @@ public class HabitStatsActivity extends AppCompatActivity {
                 // Get habit from database
                 habit = HabitRealmManager.getHabit(habitId);
 
-                if(habit == null)
+                if (habit == null)
                     return;         // TODO: forse ci andrebbe meglio un'eccezione
 
                 // Set Activity's title
@@ -79,7 +95,7 @@ public class HabitStatsActivity extends AppCompatActivity {
                 events.add(new EventDay(startDate, startIcon));
 
                 // Mark the end date if available
-                if(habit.getGoalType() == GoalType.DEADLINE){
+                if (habit.getGoalType() == GoalType.DEADLINE) {
                     Calendar endDate = Calendar.getInstance();
                     endDate.setTimeInMillis(habit.getFinalDate());
                     calendarView.setMaximumDate(endDate);
@@ -95,14 +111,6 @@ public class HabitStatsActivity extends AppCompatActivity {
 //                highlight.add(cal);
 
 
-
-
-
-
-
-
-
-
                 calendarView.setEvents(events);
                 calendarView.setHighlightedDays(highlight);
 
@@ -114,7 +122,6 @@ public class HabitStatsActivity extends AppCompatActivity {
             });
 
 
-
         }
 
 
@@ -122,12 +129,67 @@ public class HabitStatsActivity extends AppCompatActivity {
 
 
     // TODO: i dati devono essere ordinati per numero di azioni in modo crescente
+    // TODO: max 4
     private void drawStreakChart(Habit habit) {
 
+        BarChart chart = findViewById(R.id.streakChart);
 
+        ArrayList<BarEntry> entries = new ArrayList<>();
 
+        entries.add(new BarEntry(0f, 10f));
+        entries.add(new BarEntry(1f, 22f));
+        entries.add(new BarEntry(2f, 5f));
+        entries.add(new BarEntry(3f, 11f));
 
+        final String[] labels = {"22 ago - 25 ago", "27 ago - 31 ago", "1 set - 3 set", "4 set - 5 set"};
 
+        BarDataSet bardataset = new BarDataSet(entries, getString(R.string.days));
+
+        bardataset.setColors(ColorTemplate.JOYFUL_COLORS);
+        bardataset.setValueTextColor(getColor(R.color.primaryTextColor));
+        bardataset.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getBarLabel(BarEntry barEntry) {
+                return format.format(barEntry.getY());
+            }
+        });
+
+        BarData barData = new BarData(bardataset);
+
+        setChartAttributes(chart);
+
+        chart.animateY(2000);
+        
+        chart.getXAxis().setTextSize(7);
+        chart.getXAxis().setGranularity(1f);
+
+        chart.getXAxis().setCenterAxisLabels(true);
+        chart.getXAxis().setLabelCount(5, true);
+
+        chart.getXAxis().setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                return labels[(int) value];
+            }
+        });
+
+        chart.setData(barData);
+    }
+
+    private void setChartAttributes(@NonNull BarLineChartBase<?> chart) {
+
+        int primaryTextColor = getColor(R.color.primaryTextColor);
+
+        chart.getXAxis().setTextColor(primaryTextColor);
+        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        chart.getAxisRight().setDrawLabels(false);
+        chart.getAxisLeft().setTextColor(primaryTextColor);
+
+        chart.setPinchZoom(false);
+        chart.getLegend().setEnabled(false);
+        chart.setDoubleTapToZoomEnabled(false);
+        chart.getDescription().setEnabled(false);
     }
 
 
@@ -146,20 +208,20 @@ public class HabitStatsActivity extends AppCompatActivity {
     }
 
 
-    private List<Calendar> getPassedDays(@NonNull Habit habit){
+    private List<Calendar> getPassedDays(@NonNull Habit habit) {
 
         return null;
     }
 
-    private List<Calendar> getSnoozedDays(@NonNull Habit habit){
+    private List<Calendar> getSnoozedDays(@NonNull Habit habit) {
 
         return null;
     }
 
-    private Drawable getIcon(int drawableResId, int colorResId){
+    private Drawable getIcon(int drawableResId, int colorResId) {
 
         Drawable icon = ContextCompat.getDrawable(this, drawableResId);
-        if(icon != null)
+        if (icon != null)
             icon.setTint(getResources().getColor(colorResId, getTheme()));
 
         return icon;
